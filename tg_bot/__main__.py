868536252f2fb7a6cd25c4869a2ca3f9f5df1180
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-08-13 21:32:41 +0530

    __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -27,7 +27,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 **Hello {}, My Name is {}!** 
-I am an Devil  group management bot.
+I am an Devil  group management bot!.
 You can find the list of available commands with /help.
 
 """
